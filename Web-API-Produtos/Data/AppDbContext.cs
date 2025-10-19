2ecd06881a700cc089b7cf418eca937ed001d0f4
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-10-19 09:50:40 -0300

    init: initial commit

---
+++ Web-API-Produtos/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿//Meio de campo entre o DB e o código C#
+//Auxilia a fazer a conexão
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Web_API_Produtos.Models;
+
+namespace Web_API_Produtos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProdutoModel> Produtos {get; set;}
+    }
+}
