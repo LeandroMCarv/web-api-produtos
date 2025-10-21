1506737dd6d16f686477fc4113058106c7b9f26d
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-10-21 06:50:56 -0300

    feat: atualizações da string de conexão

--- Web-API-Produtos/Program.cs
+++ Web-API-Produtos/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{//usando sql server. Entrando dentro do appsettings. Pegou a connection string
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefautlConnection");
+});
 
 var app = builder.Build();
 
